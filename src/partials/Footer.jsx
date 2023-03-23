543ce44884bb6a561b4d52b4a861f4c223a14445
Emi <emi.lng711@gmail.com>
2023-03-23 13:04:06 -0300

    Update Footer.jsx

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBIcon,
-  MDBBtn,
-} from "mdb-react-ui-kit";
+import { MDBFooter, MDBContainer, MDBIcon, MDBBtn } from "mdb-react-ui-kit";
 
 export default function App() {
   return (
