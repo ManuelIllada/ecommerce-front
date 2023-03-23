81773aebd8628be8c51387fa617436edece15998
Emi <emi.lng711@gmail.com>
2023-03-23 14:58:24 -0300

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,41 +6,34 @@ import Footer from "../partials/Footer";
 function home() {
   return (
     <>
-      <section className="px-3">
-        <NavBar />
-      </section>
       {/* Hero */}
-      <section>
-        <div
-          className="p-5 text-center bg-image mt-5"
-          style={{
-            backgroundImage:
-              "url('https://mdbootstrap.com/img/new/slides/041.webp')",
-            height: "20rem",
-          }}
-        >
-          <div
-            className="mask"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
-          >
-            <div className="d-flex justify-content-center align-items-center h-100">
-              <div className="text-white">
-                <h1 className="mb-3">So Flow</h1>
-                <h4 className="mb-3">
-                  Electric mobility with pure driving pleasure!
-                </h4>
-                <a
-                  className="btn btn-outline-light btn-lg"
-                  href="#!"
-                  role="button"
-                >
-                  Discover
-                </a>
-              </div>
+      <NavBar />
+      <div
+        className="p-5 text-center bg-image"
+        style={{
+          backgroundImage:
+            "url('https://i1.wp.com/www.soflow.com/app/uploads/2022/09/Soflow_Conda.jpg?ssl=1')",
+          height: "69rem",
+        }}
+      >
+        <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
+          <div className="d-flex justify-content-center align-items-center h-100">
+            <div className="text-white">
+              <h1 className="mb-3">So Flow</h1>
+              <h4 className="mb-3">
+                Electric mobility with pure driving pleasure!
+              </h4>
+              <a
+                className="btn btn-outline-light btn-lg"
+                href="#!"
+                role="button"
+              >
+                Discover
+              </a>
             </div>
           </div>
         </div>
-      </section>
+      </div>
 
       <h1 className="text-center my-5">Our Products</h1>
       <div>
--- src/partials/Navbar.jsx
+++ src/partials/Navbar.jsx
@@ -22,52 +22,53 @@ function NavBar() {
   }, []);
 
   return (
-    <div className="container">
+    <div className="container ">
       <Navbar
-        className="navbar navbar-expand-md navbar-expand-lg fixed-top navbar-scroll"
-        bg="dark"
+        className="navbar navbar-expand-md navbar-expand-lg fixed-top"
+        bg="transparent"
+        style={{ height: "3rem" }}
         expand="lg"
       >
-        <Container fluid>
-          <Navbar.Brand href="#">SoFlow</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              {category.map((item) => (
-                <Link
-                  state={item}
-                  key={item.id}
-                  to={`/category/${item.name}`}
-                  className="text-decoration-none ms-2"
-                >
-                  {item.name}
-                </Link>
-              ))}
-            </Nav>
-            <div className="d-flex">
-              <Form className="d-flex me-3">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
+        <Navbar.Brand className="text-white mx-5" href="#">
+          SoFlow
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="navbarScroll "
+          style={{ backgroundColor: "white" }}
+        />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0 " navbarScroll>
+            {category.map((item) => (
+              <Link
+                state={item}
+                key={item.id}
+                to={`/category/${item.name}`}
+                className="text-decoration-none ms-2 text-white"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </Nav>
+          <div className="d-flex">
+            <Form className="d-flex me-3">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">
+                <img
+                  src="https://images.vexels.com/media/users/3/143356/isolated/preview/64e14fe0195557e3f18ea3becba3169b-buscar-lupa.png"
+                  alt=""
+                  style={{ height: "1.5rem", outlineColor: "white" }}
+                  variant="outline-light"
                 />
-                <Button variant="outline-success">
-                  <img
-                    src="https://images.vexels.com/media/users/3/143356/isolated/preview/64e14fe0195557e3f18ea3becba3169b-buscar-lupa.png"
-                    alt=""
-                    style={{ height: "1.5rem" }}
-                  />
-                </Button>
-              </Form>
-              <Cart />
-            </div>
-          </Navbar.Collapse>
-        </Container>
+              </Button>
+            </Form>
+            <Cart />
+          </div>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
