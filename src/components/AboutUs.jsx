9420e59089e0a9a145edb457fd179317627a40dd
Oco <nachodecores@hotmail.com>
2023-03-24 10:24:05 -0300

    aboutus component

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -15,13 +15,56 @@ function AboutUs() {
             "url('https://mdbootstrap.com/img/new/slides/041.webp')",
           height: "20rem",
         }}
-      >
-        <div>
-          <h3>Se usaron tal y cual tecnología</h3>
-        </div>
-      </div>
+      ></div>
       <div className="d-flex justify-content-center my-3">
-        <h1>Our Products</h1>
+        <h1>The Dream Team</h1>
+      </div>
+      <div>
+        <h3>Some of the implemented technologies...</h3>
+        <div>
+          <object
+            data="../logos/bootstrap.svg"
+            width="80"
+            height="80"
+            aria-label="bootstrap"
+          ></object>
+          <object
+            data="../logos/express.svg"
+            width="100"
+            height="100"
+            aria-label="express"
+          ></object>
+          <object
+            data="../logos/javascript.svg"
+            width="80"
+            height="80"
+            aria-label="javascript"
+          ></object>
+          <object
+            data="../logos/mysql.svg"
+            width="100"
+            height="100"
+            aria-label="mysql"
+          ></object>
+          <object
+            data="../logos/nodejs.svg"
+            width="100"
+            height="100"
+            aria-label="nodejs"
+          ></object>
+          <object
+            data="../logos/react.svg"
+            width="90"
+            height="90"
+            aria-label="react"
+          ></object>
+          <object
+            data="../logos/sequelize.svg"
+            width="100"
+            height="100"
+            aria-label="sequelize"
+          ></object>
+        </div>
       </div>
       <div className="d-flex justify-content-center ">
         <ProductList />
