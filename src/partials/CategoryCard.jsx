23293336d23bd364ae8d3772561a7b1bec67d431
Emi <emi.lng711@gmail.com>
2023-03-24 12:44:27 -0300

    CategoryCard

--- src/partials/CategoryCard.jsx
+++ src/partials/CategoryCard.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/esm/Button";
+import { Link } from "react-router-dom";
 
 function CategoryCard({ category }) {
   return (
     <div className="contect ">
-      <Card
-        className="bg-image hover-zoom hover-shadow"
-        style={{ width: "18rem" }}
-      >
-        <img
-          variant="top"
-          src="https://www.soflow.com/app/uploads/blogs/9/2020/09/2-600x1011.jpg"
-        />
-        <div className="mask d-flex justify-content-center align-items-center h-100 mt-5">
-          <Button variant="primary">{category.name}</Button>
-        </div>
-      </Card>
+      <Link state={category} to={`/${category.slug}`}>
+        <Card
+          className="bg-image hover-zoom hover-shadow"
+          style={{ width: "18rem" }}
+        >
+          <img variant="top" src={category.cardImage} />
+          <div className="mask d-flex justify-content-center align-items-center h-100 mt-5">
+            <Button variant="primary">{category.name}</Button>
+          </div>
+        </Card>
+      </Link>
     </div>
   );
 }
