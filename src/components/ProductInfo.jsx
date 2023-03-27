4355a430e436786dffc90fe880f10143e2dcbc39
pablinuxx <74331843+pablinuxx@users.noreply.github.com>
2023-03-27 00:54:31 -0300

    carousel con imagenes en ProductInfo

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -3,10 +3,9 @@ import { useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
 import NavBar from "../partials/Navbar";
 import { addtoCart } from "../redux/productsCartSlice";
-/* import Zoom from "react-img-hover-zoom"; */
 import "./ProductInfo.css";
 import { useState } from "react";
-import { Modal } from "react-bootstrap";
+import { Modal, Carousel } from "react-bootstrap";
 
 const ProductInfo = () => {
   const [show, setShow] = useState(false);
@@ -17,6 +16,10 @@ const ProductInfo = () => {
   const location = useLocation();
   const { name, description, media, price } = location.state;
   const [showedImage, setShowedImage] = useState(media[0]);
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
   const handleAddToCart = (item) => {
     dispatch(addtoCart(item));
@@ -27,7 +30,7 @@ const ProductInfo = () => {
   };
   return (
     <>
-      <section className="bg-dark row">
+      <section className=" row">
         <section>
           <NavBar />
         </section>
@@ -39,7 +42,7 @@ const ProductInfo = () => {
                   {media.map((item) => (
                     <div
                       key={item}
-                      className="preView border-1 rounded"
+                      className="preView border-1 rounded p-2"
                       onMouseOver={() => handleClick(item)}
                     >
                       <img src={item} alt={name} className="img-fluid" />
@@ -74,8 +77,25 @@ const ProductInfo = () => {
         </section>
       </section>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Body>Aca va la foto</Modal.Body>
+      <Modal show={show} onHide={handleClose} media={media}>
+        <Modal.Body>
+          <Carousel
+            activeIndex={index}
+            onSelect={handleSelect}
+            indicators="false"
+          >
+            {media.map((item) => (
+              <Carousel.Item key={item}>
+                <img
+                  className="d-block w-100"
+                  /* src={iteratorSymbol} */
+                  src={item}
+                  alt="First slide"
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Modal.Body>
       </Modal>
     </>
   );
