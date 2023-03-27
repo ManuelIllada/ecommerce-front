621402938ab2b1c9da526b22cee2baa9ff7fb432
pablinuxx <74331843+pablinuxx@users.noreply.github.com>
2023-03-27 15:39:50 -0300

    diseño de carrito

--- src/partials/Cart.jsx
+++ src/partials/Cart.jsx
@@ -33,11 +33,12 @@ function Cart({ name, ...props }) {
         <Offcanvas.Body>
           {stateCart &&
             stateCart.map((item) => (
-              <div key={item.id} className="d-flex align-items-center">
+              <div key={item.id} className="d-flex align-items-center mb-2">
                 <img
-                  src={item.media}
+                  src={item.media[0]}
+                  className="img-fluid"
                   alt={item.name}
-                  style={{ height: "10rem" }}
+                  style={{ height: "5rem" }}
                 />
                 <div className="ms-4 mt-3">
                   <h5>{item.name}</h5>
@@ -52,7 +53,6 @@ function Cart({ name, ...props }) {
             ))}
           <hr />
 
-          <div className="d-flex align-items-end">...</div>
           <Link to={"/cart"}>
             <Button onClick={handleShow} className="me-2" variant="">
               <svg
