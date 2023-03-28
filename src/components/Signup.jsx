883426a2cbbef5b7130f1b928193eeb643678d90
ManuelIllada <ignacio.illada@fing.edu.uy>
2023-03-28 12:42:12 -0300

    signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link /* , useNavigate */ } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 /* import { useSelector } from "react-redux"; */
 import axios from "axios";
@@ -13,7 +13,7 @@ function SignUp() {
   const [inputPhone, setInputPhone] = useState("");
   const [inputAvatar, setInputAvatar] = useState("");
   /* const user = useSelector((state) => state.user); */
-  /* const navigate = useNavigate(); */
+  const navigate = useNavigate();
   const handleSubmit = async (event) => {
     event.preventDefault();
     const response = await axios({
@@ -24,18 +24,18 @@ function SignUp() {
       }, */
       url: `${process.env.REACT_APP_API_URL}/users`,
       data: {
-        inputFirstname,
-        inputLastname,
-        inputEmail,
-        inputAddress,
-        inputPassword,
-        inputPhone,
-        inputAvatar,
+        firstname: inputFirstname,
+        lastname: inputLastname,
+        email: inputEmail,
+        address: inputAddress,
+        password: inputPassword,
+        phone: inputPhone,
+        avatar: inputAvatar,
       },
     });
     console.log("ola");
     console.log(response);
-    /* navigate("/login"); */
+    navigate("/login");
   };
 
   return (
