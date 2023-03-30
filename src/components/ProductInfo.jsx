39e1224b62b45b8c043913a0ef9abbef22477ead
juan-abraham-dv <123184630+juan-abraham-dv@users.noreply.github.com>
2023-03-30 12:14:40 -0300

    Prueba

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -7,7 +7,6 @@ import "./ProductInfo.css";
 import { useState } from "react";
 import { Modal, Carousel, Button } from "react-bootstrap";
 import Footer from "../partials/Footer";
-import HeroProducts from "../partials/HeroProducts";
 
 const ProductInfo = () => {
   const [show, setShow] = useState(false);
