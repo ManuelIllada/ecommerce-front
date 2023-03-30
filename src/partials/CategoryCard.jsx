35669f87ec9abf5010b5d46dc5cdc02bb4595872
Oco <nachodecores@hotmail.com>
2023-03-30 16:11:24 -0300

    titulos en las category cards

--- src/partials/CategoryCard.jsx
+++ src/partials/CategoryCard.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import "./CategoryCard.css";
 
 function CategoryCard({ category }) {
   return (
     <Link
       state={category}
-      className="m-3 bg-image rounded hover-zoom hover-shadow col-9 col-sm-5 col-lg-3 col-xl-2"
+      className="m-3 bg-image rounded hover-zoom hover-shadow hover-overlay col-9 col-sm-5 col-lg-3 col-xl-2"
       to={`/${category.slug}`}
     >
       <Card>
-        <img
-          variant="top"
-          src={category.cardImage}
-          className="img-fluid rounded"
-          alt={category.name}
-        />
+        <div className="image-container">
+          <h2 className="title">{category.name}</h2>
+          <img
+            variant="top"
+            src={category.cardImage}
+            className="img-fluid rounded"
+            alt={category.name}
+          />
+        </div>
       </Card>
+      <div
+        className="mask w-100 h-100 hover-zoom position-absolute top-0 start-0"
+        style={{ backgroundColor: "hsla(0, 0%, 0%, 0.5)" }}
+      ></div>
     </Link>
   );
 }
