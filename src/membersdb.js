19ae8ab607826904c1752de5db1ab6fb5f4682e3
Oco <nachodecores@hotmail.com>
2023-04-11 11:25:35 -0300

    links a redes

--- src/membersdb.js
+++ src/membersdb.js
@@ -5,8 +5,8 @@ const members = [
     img:
       "https://cdn.discordapp.com/attachments/852698000890658876/1088807878883807312/338eba60-b1d7-4d1d-9f18-a9d3121708e2.jpg",
     bio: "Ad lorem ipsum y alguna cosita más",
-    linkedin: "https://es.wikipedia.org/wiki/Wikipedia:Portada",
-    github: "https://github.com/",
+    linkedin: "https://www.linkedin.com/in/emilng711/",
+    github: "https://github.com/Emiliano711",
   },
   {
     firstname: "Ignacio",
@@ -14,8 +14,9 @@ const members = [
     img:
       "https://cdn.discordapp.com/attachments/852698000890658876/1088813526237315092/foto_perfil.jpg",
     bio: "Ad lorem ipsum y alguna cosita más",
-    linkedin: "https://es.wikipedia.org/wiki/Wikipedia:Portada",
-    github: "https://github.com/",
+    linkedin:
+      "https://www.linkedin.com/in/ignacio-jos%C3%A9-de-cores-damiani-4b6b86b7/",
+    github: "https://github.com/nachodecores",
   },
   {
     firstname: "Juan",
@@ -23,8 +24,8 @@ const members = [
     img:
       "https://cdn.discordapp.com/attachments/852698000890658876/1088808651646570617/Untitled.png",
     bio: "Ad lorem ipsum y alguna cosita más",
-    linkedin: "https://es.wikipedia.org/wiki/Wikipedia:Portada",
-    github: "https://github.com/",
+    linkedin: "https://www.linkedin.com/in/juan-abraham/",
+    github: "https://github.com/juan-abraham-dv",
   },
   {
     firstname: "Manuel",
@@ -32,8 +33,8 @@ const members = [
     img:
       "https://cdn.discordapp.com/attachments/852698062668955739/1089920200633241600/qwey.png",
     bio: "Ad lorem ipsum y alguna cosita más",
-    linkedin: "https://es.wikipedia.org/wiki/Wikipedia:Portada",
-    github: "https://github.com/",
+    linkedin: "https://www.linkedin.com/in/manuillada/",
+    github: "https://github.com/ManuelIllada",
   },
   {
     firstname: "Pablo",
